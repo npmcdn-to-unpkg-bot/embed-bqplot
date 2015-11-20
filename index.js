7701c8b46914fb69a507304cc8918ff9662ffd1c
Sylvain Corlay <sylvain.corlay@gmail.com>
2015-11-19 23:56:26 -0500

    Rename json state

--- index.js
+++ index.js
@@ -3,14 +3,9 @@ var WidgetManager = require("./manager").WidgetManager;
 
 document.addEventListener("DOMContentLoaded", function(event) {
 
-    var top_level_ids = [
-        "6fc69e2dca814ef0a07c84d84585e27b", 
-        "43621447f4434bfeb95e36e7a15986ff"
-    ];
-
     var manager = new WidgetManager(document.body);
 
-    var state = require("./spx_correlation.json");
+    var state = require("./widget_state.json");
 
     manager.set_state(state);
 });
